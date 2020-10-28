79fb96d7b20a07416961c189410af68fab0d9932
yuandabo <540496776@qq.com>
2020-10-28 16:15:43 +0800

    reacr react-transition-group

--- reactDemo/eventdemo/src/App.js
+++ reactDemo/eventdemo/src/App.js
@@ -1,71 +1,97 @@
-import React, { Component, Fragment } from 'react'
-import './main.css'
-import XiaojiejieItem from './XiaojiejieItem'
-import axios from 'axios'
+import React, { Component, Fragment } from "react";
+import {
+    CSSTransition,
+    TransitionGroup,
+  } from 'react-transition-group';
+import "./main.css";
+import XiaojiejieItem from "./XiaojiejieItem";
+import axios from "axios";
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      value: '',
-      itemList: [{ value: '吃快餐' }, { value: '吃点餐' }, { value: '吃中式餐' }],
-      fetchData:''
-    }
+      value: "",
+      itemList: [
+        { value: "吃快餐" },
+        { value: "吃点餐" },
+        { value: "吃中式餐" },
+      ],
+      fetchData: "",
+    };
   }
-  componentDidMount(){
-    this.fetchData()
+  componentDidMount() {
+    this.fetchData();
   }
   render() {
     return (
       <Fragment>
-        <input className="input"
-         value={this.state.value}
+        <input
+          className="input"
+          value={this.state.value}
           onChange={this.inputClick.bind(this)}
-          ref={(input=>{this.input = input})}
-           />
-        <button onClick={this.addItem.bind(this)}>添加</button>
-        <ul ref={ul=>this.ul=ul}>
-          {
-            this.state.itemList.map((v, index) => {
+          ref={(input) => {
+            this.input = input;
+          }}
+        />
+        <button onClick={this.addItem.bind(this)}> 添加 </button>
+        <ul ref={(ul) => (this.ul = ul)}>
+          <TransitionGroup>
+            {this.state.itemList.map((v, index) => {
               return (
-                <XiaojiejieItem key={index+v.value} content={v} itemIndex={index} delItem={this.delItem.bind(this)}></XiaojiejieItem>
-              )
-            })
-          }
+                <CSSTransition
+                  timeout={2000}
+                  classNames="boss-text"
+                  unmoutOnExit
+                  key={v + index}
+                >
+                  <XiaojiejieItem
+                    key={index + v.value}
+                    content={v}
+                    itemIndex={index}
+                    delItem={this.delItem.bind(this)}
+                  ></XiaojiejieItem>
+                </CSSTransition>
+              );
+            })}
+          </TransitionGroup>
         </ul>
-        <div>
-          fetchdata:{this.state.fetchData}
-        </div>
+        <div>fetchdata: {this.state.fetchData} </div>
       </Fragment>
-    )
+    );
   }
   inputClick(e) {
     this.setState({
-      value: this.input.value
-    })
+      value: this.input.value,
+    });
   }
   addItem() {
-    console.log(this.state)
-    this.setState({
-      itemList: [...this.state.itemList, { value: this.state.value }],
-      value: ''
-    },()=>{console.log(this.ul.querySelectorAll('li').length)})
+    console.log(this.state);
+    this.setState(
+      {
+        itemList: [...this.state.itemList, { value: this.state.value }],
+        value: "",
+      },
+      () => {
+        console.log(this.ul.querySelectorAll("li").length);
+      }
+    );
   }
   delItem(itemIndex) {
-    const list = this.state.itemList.filter((v,index) => {
-      return index !== itemIndex
-    })
+    const list = this.state.itemList.filter((v, index) => {
+      return index !== itemIndex;
+    });
     this.setState({
-      itemList: list
-    })
+      itemList: list,
+    });
   }
-  fetchData(){
-    axios.get('http://localhost:1234/eventdemo').then(res=>{
-      console.log(res)
+  fetchData() {
+    axios.get("http://localhost:1234/eventdemo").then((res) => {
+      console.log(res);
       this.setState({
-        fetchData:res.data
-      })
-    })
+        fetchData: res.data,
+      });
+    });
   }
 }
 
-export default App
+export default App;
