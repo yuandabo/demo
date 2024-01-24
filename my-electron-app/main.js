df518c90c9ad796524636d95288b4b8af11d7255
yuandabo <540496776@qq.com>
2024-01-24 17:34:14 +0800

    electron

---
+++ my-electron-app/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron')
+// 在你文件顶部导入 Node.js 的 path 模块
+const path = require('node:path')
+
+// 修改已有的 createWindow() 方法
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+// ...
+
+app.whenReady().then(() => {
+    createWindow()
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
