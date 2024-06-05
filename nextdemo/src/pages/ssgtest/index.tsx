2a9b1d673da8e8295899d8a2bf5ff98f1dfffd6a
yuandabo <540496776@qq.com>
2024-06-05 09:57:06 +0800

    feat: next14demo

---
+++ nextdemo/src/pages/ssgtest/index.tsx
@@ -0,0 +1,11 @@
+export async function getStaticProps() {
+    return {
+      props: {}, // will be passed to the page component as props
+    }
+  }
+
+export default function Ssgtest () {
+    return <>test ssg</>
+}
+
+
---
+++ nextdemo/src/pages/ssrtest/index.tsx
@@ -0,0 +1,11 @@
+export async function getServerSideProps() {
+    return {
+      props: {}, // will be passed to the page component as props
+    }
+  }
+
+export default function Ssrtest () {
+    return <>test Ssrtest</>
+}
+
+
